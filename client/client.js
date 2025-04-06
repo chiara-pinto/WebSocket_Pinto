08e2c85750c9db17d7410939812e636dc960fad6
Chiara Pinto <pintochiara@itis-molinari.eu>
2025-04-06 21:52:03 +0200

    completo client

--- client/client.js
+++ client/client.js
@@ -1,37 +1,61 @@
-const input = document.getElementById("input");
-const button = document.getElementById("sendButton");
+const campoNome = document.getElementById("campoNome");
+const bottoneNome = document.getElementById("bottoneNome");
+const campoMessaggio = document.getElementById("campoMessaggio");
+const bottoneInvia = document.getElementById("bottoneInvia");
 const chat = document.getElementById("chat");
+const utenti = document.getElementById("utenti");
 
-const template = "<li class=\"list-group-item\">%MESSAGE</li>";
-const messages = [];
+const modelloMessaggio = "<li class=\"list-group-item\">%MESSAGGIO</li>";
+const modelloUtente = "<li class=\"list-group-item\">%NOME</li>";
+const messaggi = [];
 
-const socket = i0 ();
+const socket = io();
 
-input.onkeydown = (event) => {
-  
-  if (event.keyCode === 13) {
-      event.preventDefault();
-      button.click();
+const mostraModale = new bootstrap.Modal(document.getElementById("modaleNome"));
+mostraModale.show();
+
+bottoneNome.onclick = () => {
+  const nome = campoNome.value;
+  socket.emit("nome", nome);
+  mostraModale.hide();
+}
+
+campoMessaggio.onkeydown = (evento) => {
+  if (evento.keyCode === 13) {
+    evento.preventDefault();
+    bottoneInvia.click();
   }
 }
 
-button.onclick = () => {
-  socket.emit("message", input.value);
-  input.value = "";
+bottoneInvia.onclick = () => {
+  socket.emit("messaggio", campoMessaggio.value);
+  campoMessaggio.value = "";
 }
 
-socket.on("chat", (message) => {
-  console.log(message);
-  messages.push(message);
-  render();
-})
+socket.on("chat", (messaggio) => {
+  messaggi.push(messaggio);
+  disegnaChat();
+});
 
-const render = () => {
+socket.on("lista", (lista) => {
+  disegnaLista(lista);
+});
+
+const disegnaChat = () => {
   let html = "";
-  messages.forEach((message) => {
-    const row = template.replace("%MESSAGE", message);
-    html+=row;
+  messaggi.forEach((m) => {
+    const riga = modelloMessaggio.replace("%MESSAGGIO", m);
+    html += riga;
   });
   chat.innerHTML = html;
   window.scrollTo(0, document.body.scrollHeight);
+}
+
+const disegnaLista = (lista) => {
+  let html = "";
+  lista.forEach((u) => {
+    const riga = modelloUtente.replace("%NOME", u.nome);
+    html += riga;
+  });
+  utenti.innerHTML = html;
 }
